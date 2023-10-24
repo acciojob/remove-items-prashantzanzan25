5562e2c54b94e95227a032d712db2de43b7239ef
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-10-24 11:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor{
+	const colorSelect = document.getElementById('colorSelect');
+	const selectedOption = colorSelect.options(colorSelect.selectedIndex);
+
+	if(selectedOption){
+		colorSelect.removechild(selectedOption);
+	}
+}
